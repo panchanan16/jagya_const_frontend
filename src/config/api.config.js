6bdb2e24733a951bfa7a020d787035701e856556
msi ( Aditya ) <108588359+msi-end@users.noreply.github.com>
2025-10-22 17:54:59 +0530

    Update api.config.js

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3500/'
+export const BASE_URL = 'https://api.reynrel.in/'
 export const API_ENTITY = 'api/v1/entity/'
 export const API_CORE = 'api/v1/core/'
 export const API_AUTH = 'api/auth/'
